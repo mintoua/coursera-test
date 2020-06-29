88942ef671fc1f68e1c10eeae2aa8d6408219fad
mintoua <47702828+mintoua@users.noreply.github.com>
2020-06-29 02:42:22 +0100

    Update

--- mod5_solution/js/ajax-utils.js
+++ mod5_solution/js/ajax-utils.js
@@ -6,10 +6,10 @@ var ajaxUtils = {};
 
 // Returns an HTTP request object
 function getRequestObject() {
-  if (window.XMLHttpRequest) {
+  if (global.XMLHttpRequest) {
     return (new XMLHttpRequest());
   } 
-  else if (window.ActiveXObject) {
+  else if (global.ActiveXObject) {
     // For very old IE browsers (optional)
     return (new ActiveXObject("Microsoft.XMLHTTP"));
   } 
@@ -64,3 +64,4 @@ global.$ajaxUtils = ajaxUtils;
 
 
 })(window);
+
